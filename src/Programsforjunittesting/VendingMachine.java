1297a128e7ad1d8f0475dc72c6f5865e77e143e0
palle146github <prasanthpalle146@gmail.com>
2023-01-09 12:57:53 +0530

    Day6 Logical Programming Practice Problem

--- src/Programsforjunittesting/VendingMachine.java
+++ src/Programsforjunittesting/VendingMachine.java
@@ -6,11 +6,11 @@ import java.util.Arrays;
 
 public class VendingMachine {
     public static void countCurrency(int amount) {
-        int[] notes = new int[]{2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
-        int[] noteCounter = new int[10];
+        int[] notes = new int[]{1000, 500, 100, 50, 10, 5, 2, 1};
+        int[] noteCounter = new int[8];
 
         // count notes using Greedy approach
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             if (amount >= notes[i]) {
                 noteCounter[i] = amount / notes[i];
                 amount = amount - noteCounter[i] * notes[i];
@@ -18,14 +18,14 @@ public class VendingMachine {
         }
         // Print notes
         System.out.println("Currency Count ->");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 8; i++) {
             if (noteCounter[i] != 0) {
                 System.out.println(notes[i] + " : "
                         + noteCounter[i]);
             }
         }
     }
-    public static void main(String argc[]) {
+    public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter amount");
         int amount = s.nextInt();
